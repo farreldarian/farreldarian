dfaf1d454e4642ea0a86e87bafe2494da403e3c7
farreldarian <farreldarian@users.noreply.github.com>
2023-04-16 01:45:27 +0700

    Create HeroSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
 export default function IndexPage() {
-  return <div></div>
+  return (
+    <main className='min-h-screen grid place-content-center'>
+      <div className='max-w-4xl mx-auto'>
+        <section className='text-2xl space-y-6'>
+          <p>
+            Farrel Darian, crafting interfaces that abstract complexity.
+            Seamlessly bleding art with cutting-edge technology, pushing the
+            boundaries of what’s possible.
+          </p>
+          <p>
+            Innovating as an experienced full-stack engineer, specializing in
+            blockchain and web technology, educated in computer science and AI.
+          </p>
+          <p>Currently building Netra as CTO. Previously at GDP Labs.</p>
+          <p>Based in Jakarta with global mindset.</p>
+        </section>
+      </div>
+    </main>
+  )
 }
